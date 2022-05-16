0e362fb15b90368a28ae631aa6d88759bbaf8377
s13g <ayflix0@gmail.com>
2022-05-16 14:51:52 +0100

    ORM done

--- tags/models.py
+++ tags/models.py
@@ -6,11 +6,18 @@ from django.db import models
 # Create your models here.
 
 
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+        return TaggedItem.objects.select_related('tags').filter(content_type=content_type, object_id=obj_id)
+
+
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
 
 class TaggedItem(models.Model):
+    objects = TaggedItemManager()
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
