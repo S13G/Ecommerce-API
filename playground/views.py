aef18a2c9276cfb33ab08bdd099eb3d8a3c48f8c
s13g <ayflix0@gmail.com>
2022-05-16 10:13:45 +0100

    ORM

---
+++ playground/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def say_hello(request):
+    return render(request, 'hello.html', {'name': 'Sieg'})
